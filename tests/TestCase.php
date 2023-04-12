a2519e3526dffbb38101d010b6df700fe179ada1
Piotrek <piotr.blaszkowski@winlocal.com>
2023-04-12 09:39:34 +0200

    Test Update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,19 +6,8 @@ use WinLocal\MessageBus\MessageBusServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    /**
-     * Automatically enables package discoveries.
-     *
-     * @var bool
-     */
-    protected $enablesPackageDiscoveries = true;
+    protected $loadEnvironmentVariables = false;
 
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array<int, class-string>
-     */
     protected function getPackageProviders($app)
     {
         return [MessageBusServiceProvider::class];
