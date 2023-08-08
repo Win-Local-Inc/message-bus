774b752b99f6914675186a1f1a253c122a44b0d2
Piotrek <piotr.blaszkowski@winlocal.com>
2023-08-08 11:27:02 +0200

    Subject Updates

--- src/Enums/Subject.php
+++ src/Enums/Subject.php
@@ -26,21 +26,37 @@ enum Subject: string
     case DevicesOrderPaid = 'devices.order.paid';
     case DeviceUsed = 'device.used';
 
+    case LeaderBoardCreated = 'leaderboard.created';
+    case LeaderBoardDisabled = 'leaderboard.disabled';
+    case LeaderBoardEnabled = 'leaderboard.enabled';
+    case LeaderBoardStarted = 'leaderboard.started';
+
     case FacebookAudienceCreated = 'audience.facebook.created';
     case FacebookAudienceUpdated = 'audience.facebook.updated';
     case FacebookAudienceDeleted = 'audience.facebook.deleted';
     case FacebookAdvertCreated = 'advert.facebook.created';
     case FacebookAdvertUpdated = 'advert.facebook.updated';
     case FacebookAdvertInsightsReceived = 'advert.facebook.insights.received';
+    case FacebookCampaignActivated = 'campaigns.facebook.activated';
+    case FacebookCampaignEnded = 'campaigns.facebook.ended';
+    case FacebookCampaignFailed = 'campaigns.facebook.failed';
 
     case MbcSharecardCreated = 'mbc.sharecard.created';
     case MbcSharecardShared = 'mbc.sharecard.shared';
     case MbcSharecardShareOwnerNotified = 'mbc.sharcard.share.owner.notified';
     case MbcSharecardUpdated = 'mbc.sharecard.updated';
     case MbcSharecardVisited = 'mbc.sharecard.visited';
+    case MbcSharecardTemplateUnsharedFromUser = 'mbc.sharecard.template.unshared_from_user';
+    case MbcSharecardTemplateSharedToUser = 'mbc.sharecard.template.shared_to_user';
 
     case MediaShared = 'media.shared';
     case MediaCatalogShared = 'media.catalog.shared';
+
+    case PlayerAttachedToLeaderBoard = 'leaderboard.player.attached_to_leaderboard';
+    case PlayerDetachedFromLeaderBoard = 'leaderboard.player.detached_from_leaderboard';
+
+    case ShareLinkShared = 'sharelink.shared';
+    case ShareLinkUnshared = 'sharelink.unshared';
 
     case ShortlinkCreated = 'shortlink.created';
     case ShortlinkEnteredUnique = 'shortlink.entered.unique';
@@ -58,6 +74,7 @@ enum Subject: string
     case UserStatusActivated = 'user.status.activated';
     case UserWorkspaceInvited = 'user.workspace.invited';
     case UserWorkspaceMoved = 'user.workspace.moved';
+    case UserOnboarded = 'user.onboarded';
 
     case WorkspaceCreated = 'auth.workspace.created';
     case WorkspaceUpdated = 'auth.workspace.updated';
@@ -66,7 +83,10 @@ enum Subject: string
     case WorkspaceStatusActivated = 'workspace.status.activated';
     case WorkspaceStatusDeactivated = 'workspace.status.deactivated';
     case WorkspaceSubscriptionEnded = 'workspace.subscription.ended';
-    case WorkspaceSubscriptionExpiringNotified = 'workspace.subscription.expiring.notified';
+    case WorkspaceSubscriptionExpiring = 'workspace.subscription.expiring';
     case WorkspaceSubscriptionCreated = 'workspace.subscription.created';
     case WorkspaceSubscriptionUpdated = 'workspace.subscription.updated';
+    case WorkspaceSubscriptionRenewalFailed = 'workspace.subscription.failed';
+    case WorkspaceSubscriptionRenewalSucceeded = 'workspace.subscription.succeeded';
+
 }
